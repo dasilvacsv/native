494928f49d122e51aeff4ad3ffabe1e325990771
dasilvacsv <dasilva.csv@gmail.com>
2024-07-28 19:22:40 -0400

    Configuracion del linter y prettier

--- App.js
+++ App.js
@@ -1,38 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, Image, Pressable, StyleSheet, Text, View } from 'react-native';
-const icon = require('./assets/icon.png')
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{uri:"https://www.metacritic.com/a/img/resize/3bc917343b798369b95f16269ad0953da023f5f4/catalog/provider/6/12/6-1-1008938-52.jpg?auto=webp&fit=cover&height=300&width=200"}} style={{width: 215, height: 294}} />
-      <Text style={{color:'white'}}>Tenemos aquí la configuración</Text>
-      <StatusBar style="light" />
-      <Button title='Pulsa aqui' onPress={() => alert('Hola')}/>
-      <Pressable
-        onPress={() => {
+      <Image
+        source={{
+          uri: "https://www.metacritic.com/a/img/resize/3bc917343b798369b95f16269ad0953da023f5f4/catalog/provider/6/12/6-1-1008938-52.jpg?auto=webp&fit=cover&height=300&width=200",
         }}
-        style={({pressed}) => [
+        style={{ width: 215, height: 294 }}
+      />
+      <Text style={{ color: "white" }}>Tenemos aquí la configuración</Text>
+      <StatusBar style="light" />
+      <Button title="Pulsa aqui" onPress={() => alert("Hola")} />
+      <Pressable
+        onPress={() => {}}
+        style={({ pressed }) => [
           {
-            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+            backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
           },
           styles.wrapperCustom,
-        ]}>
-        {({pressed}) => (
-          <Text style={
-            {fontWeight: pressed ? 'bold' : 'italic'}
-          }>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+        ]}
+      >
+        {({ pressed }) => (
+          <Text style={{ fontWeight: pressed ? "bold" : "italic" }}>
+            {pressed ? "Pressed!" : "Press Me"}
+          </Text>
         )}
       </Pressable>
-  </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
