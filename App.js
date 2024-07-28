88ee8b950d2689d3dd1aa2ed16e415ed5caad0c0
dasilvacsv <dasilva.csv@gmail.com>
2024-07-28 18:31:42 -0400

    conociendo los componentes, view, text, e imagenes

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+const icon = require('./assets/icon.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={icon} style={{width: 200, height: 100, resizeMode: 'contain'}} />
       <Text>Tenemos aquí la configuración</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09f',
     alignItems: 'center',
     justifyContent: 'center',
   },
